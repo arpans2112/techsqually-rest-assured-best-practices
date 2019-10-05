a5b9b1c9605d34c4fd12c97fe0086b67bd3642b3
arpans2112 <arpan673523@gmail.com>
2019-10-04 20:42:43 -0500

    ExtractingMultipleValueFromResponse.java modified

--- src/main/java/api/restassured/libarary/basics/get/gettingResponseData/ExtractingMultipleValueFromResponse.java
+++ src/main/java/api/restassured/libarary/basics/get/gettingResponseData/ExtractingMultipleValueFromResponse.java
@@ -2,8 +2,12 @@ package api.restassured.libarary.basics.get.gettingResponseData;
 
 import api.zippopotam.AbstractBaseZippopotamAPIUtil;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -14,7 +18,7 @@ public class ExtractingMultipleValueFromResponse extends AbstractBaseZippopotamA
 
         /**if you have to extract one value after the validation
          *  path with single parameter*/
-        String postCode =   given().
+        Response response =   given().
                 /**Setting Request Specification in AbstractBaseAPIUtil*/
                         spec(requestSpecification).log().all().
                         when().
@@ -22,10 +26,32 @@ public class ExtractingMultipleValueFromResponse extends AbstractBaseZippopotamA
                         then().log().all().
                         statusCode(200).
                         contentType(ContentType.JSON).
-                        extract().
-                        path("'post code'");
+                        extract().response();
 
-        Assert.assertEquals(postCode,"90210");
+        /**Getting Multiple Response from the Response and validating*/
+
+        /* get direct Node*/
+        String post_Code = response.path("'post code'");
+        String country = response.path("country");
+        String countryAbbreviation = response.path("'country abbreviation'");
+
+       /**List of Map as there can be Multiple places in future*/
+        List<Map<String,Object>> places = response.path("places");
+        String placeName =  places.get(0).get("place name").toString();
+        String longitude  = places.get(0).get("longitude").toString();
+        String state = places.get(0).get("state").toString();
+        String stateAbbreviation = places.get(0).get("state abbreviation").toString();
+        String latitude = places.get(0).get("latitude").toString();
+
+        Assert.assertEquals(post_Code , "90210" );
+        Assert.assertEquals(country , "United States" );
+        Assert.assertEquals(countryAbbreviation , "US" );
+        Assert.assertEquals(placeName , "Beverly Hills" );
+        Assert.assertEquals(longitude , "-118.4065" );
+        Assert.assertEquals(state , "California" );
+        Assert.assertEquals(stateAbbreviation , "CA" );
+        Assert.assertEquals(latitude , "34.0901" );
+
 
     }
 }
