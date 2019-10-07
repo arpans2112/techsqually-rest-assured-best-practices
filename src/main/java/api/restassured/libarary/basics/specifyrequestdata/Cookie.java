c3c0a0b7b7533457f6b6c6ec9f71880b34cbbe73
arpans2112 <arpan673523@gmail.com>
2019-10-06 21:40:21 -0500

    GetResponseTime : Example is added :GetResponseTime.java Cookies Verification in Response Example is added : Cookie.java

---
+++ src/main/java/api/restassured/libarary/basics/specifyrequestdata/Cookie.java
@@ -0,0 +1,24 @@
+package api.restassured.libarary.basics.specifyrequestdata;
+
+import api.common.abstractapiutil.AbstractBaseZippopotamAPIUtil;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Cookie extends AbstractBaseZippopotamAPIUtil {
+
+
+    @Test
+    public void requestSpecificationTest(){
+        given().log().all().
+                /**Setting Request Specification in AbstractBaseAPIUtil*/
+               spec(requestSpecification).
+         when().
+                get("us/90210").
+         then().log().all().
+                statusCode(200).
+                contentType(ContentType.JSON);
+    }
+
+}
